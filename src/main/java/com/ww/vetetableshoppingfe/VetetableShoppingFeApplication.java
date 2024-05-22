6da74226fc5bf8f8a8884341cf572616244717a8
Admin <linhnguyen3041999@gmail.com>
2024-05-22 20:02:20 +0700

    first commit

---
+++ src/main/java/com/ww/vetetableshoppingfe/VetetableShoppingFeApplication.java
@@ -0,0 +1,13 @@
+package com.ww.vetetableshoppingfe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VetetableShoppingFeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VetetableShoppingFeApplication.class, args);
+	}
+
+}
