97609ee79e7e39523cd20134ec0003a5bcd61d49
Nhat Linh <nguyentungnhatlinh@fabercompany.co.jp>
2024-06-28 15:31:21 +0700

    Feature product

--- src/main/resources/static/user/js/index.js
+++ src/main/resources/static/user/js/index.js
@@ -1,51 +1,28 @@
 let token = sessionStorage.getItem('token');
 
-async function getAllProduct() {
+async function getFeatureProduct() {
   try {
     // Gọi API để lấy dữ liệu sản phẩm
     let {data: products} = await axios.get(
-        'http://localhost:8080/api/v1/products');
-    console.log(products); // Kiểm tra dữ liệu trong console
-    // Biến để lưu trữ kết quả HTML
-    let result = '';
+        'http://localhost:8080/api/v1/products/feature-product');
     products.forEach(product => {
-      result += `
-        <div class="col-xl-3 col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
-            <div class="product-item border my-3">
-                <div class="position-relative bg-light overflow-hidden">
-                    <img class="img-fluid w-100 product-image" src="https://drive.google.com/thumbnail?id=${product.photo}" alt="ImgProduct">
-                    <div
-                            class="bg-secondary rounded text-white position-absolute start-0 top-0 m-4 py-1 px-3">
-                        New
-                    </div>
+        $('#product-list').append(`
+          <div class="col-lg-3 col-md-4 col-sm-6">
+            <div class="product__item">
+                <div class="product__item__pic set-bg" style="background-image: url(${product.photo})">
+                    <ul class="product__item__pic__hover">
+                        <li><a id="add-to-cart-${product.productId}"><i class="fa fa-shopping-cart"></i></a></li>
+                    </ul>
                 </div>
-                <div class="text-center p-4">
-                    <a class="d-block h5 mb-2 product-name" href="/product/product-detail">${product.productName}</a>
-                    <span class="mr-1 product-price">$${product.price * 0.9}</span>
-                    <s class="text-body text-decoration-line-through">$${product.price}</s>
-                </div>
-                <div class="d-flex border-top  font-weight-bold">
-                    <small class="w-50 text-center border-right py-2">
-                        <a class="text-body" href="/product/product-detail"><i
-                                class="fa fa-eye mr-2"></i><span>View detail</span></a>
-                    </small>
-                    <small class="w-50 text-center py-2">
-                        <button id="add-to-cart-${product.productId}" class="text-body border-0"><i
-                                class="fa fa-shopping-bag mr-2"></i><span>Add to cart</span></button>
-                    </small>
+                <div class="product__item__text">
+                    <h6><a href="#">${product.productName}</a></h6>
+                    <h5>$${product.price}</h5>
                 </div>
             </div>
         </div>
-      `;
-    });
-    // Hiển thị kết quả lên trang HTML
-    document.getElementById('product-list').innerHTML = result;
-    let itemList = [];
-    if (localStorage.getItem("items")) {
-      itemList = JSON.parse(localStorage.getItem("items"));
-    }
-    products.forEach(product => {
-      // Add
+        `);
+
+      // Add to cart event
       let addToCart = document.getElementById(
           `add-to-cart-${product.productId}`);
       addToCart.addEventListener('click', async () => {
@@ -71,10 +48,15 @@ async function getAllProduct() {
         swal.fire("Added to cart!");
       });
     });
+
+    let itemList = [];
+    if (localStorage.getItem("items")) {
+      itemList = JSON.parse(localStorage.getItem("items"));
+    }
+
+
   } catch (error) {
     console.error('Error fetching data:', error);
-    document.getElementById(
-        'product-list').innerHTML = '<p>Error fetching data</p>';
   }
 }
 
@@ -104,7 +86,7 @@ async function loadCategories() {
 }
 
 // Gọi hàm khi trang được tải
-window.getAllProduct();
+window.getFeatureProduct();
 window.loadCategories();
 window.getAmount();
 window.getCount();
